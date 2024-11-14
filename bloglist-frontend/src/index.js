4d97d3e9b5b9a8da85820156865f8d30377bd9e6
Petri Kotiranta <petri.kotiranta@exaum.com>
2024-11-14 19:03:33 +0200

    12.22

--- bloglist-frontend/src/index.js
+++ bloglist-frontend/src/index.js
@@ -1,21 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import { AuthProvider } from "./AuthContext";
 import { NotificationProvider } from "./NotificationContext";
 
 const queryClient = new QueryClient();
+const container = document.getElementById("root");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <NotificationProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </NotificationProvider>
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// Ensure container is not null
+if (container) {
+  const root = createRoot(container); // Use createRoot instead of ReactDOM.render
+
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <NotificationProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </NotificationProvider>
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
